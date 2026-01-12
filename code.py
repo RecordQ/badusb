ee8cbef1e1ac3ef22d710da90210fb3db024770d
Darya Kardo <daryarecord@gmail.com>
2026-01-12 21:12:30 +0300

    modified:   code.py 	renamed:    payload.txt -> payloads/payload.dd

--- code.py
+++ code.py
@@ -1,6 +1,8 @@
+from glob import glob
 from badusb.command import Command
 
 # Loads and executes commands from the payload
 if __name__ == "__main__":
-    payload = open("payload.txt", "r").read()
-    Command().run(payload)
+    for file in glob("payloads/*.dd"):
+        payload = open(file, "r").read()
+        Command().run(payload)
