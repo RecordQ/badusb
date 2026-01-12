d1533d62387aabd9a51d5047b1d7f74d4a6309eb
Darya Kardo <user@quaxicron.com>
2026-01-12 19:02:22 +0300

    modified:   pico-badusb/main.py

--- pico-badusb/main.py
+++ pico-badusb/main.py
@@ -3,4 +3,4 @@ from badusb.command import Command
 # Loads and executes commands from the payload
 if __name__ == "__main__":
     payload = open("payload.txt", "r").read()
-    Command().execute(payload)
+    Command().run(payload)
