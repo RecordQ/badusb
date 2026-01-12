e6bea2881d433053f34ea3ea74e2bf803f155a07
Darya Kardo <daryarecord@gmail.com>
2026-01-12 21:15:24 +0300

    modified:   code.py

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
-from glob import glob
+import glob
 from badusb.command import Command
 
 # Loads and executes commands from the payload
 if __name__ == "__main__":
-    for file in glob("payloads/*.dd"):
+    for file in glob.glob("payloads/*.dd"):
         payload = open(file, "r").read()
         Command().run(payload)
